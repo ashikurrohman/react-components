8011e2f4356e4d4a214c7774b08021f6a2b99b76
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-01 11:38:01 +0600

    How to show search query

---
+++ SearchQueryshow.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+export default class IncDec extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            event: ''
+        }
+    }
+    eventHandelar = (event) => {
+        this.setState({
+            name: event.target.value
+       
+        })
+    }
+        render() {
+            return (
+                <div>
+                    <input onChange={this.eventHandelar} value={this.state.name} type="text" placeholder="Write Anything"></input>
+                    {this.state.name ? <p>Hello,{this.state.name}</p> : ''}
+                </div>
+            )
+        }
+    
+   
+}
