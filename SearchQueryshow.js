95a3f2b9e7e8c4a06481b956a3e32d9c4e080d57
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-01 11:53:20 +0600

    Update SearchQueryshow.js

--- SearchQueryshow.js
+++ SearchQueryshow.js
@@ -1,26 +1,22 @@
-import React,{Component} from 'react';
-
-export default class IncDec extends Component {
-    constructor(props) {
+import React, { Component } from 'react';
+export default class IncDec extends Component{
+    constructor(props){
         super(props);
         this.state = {
-            event: ''
+            event:''
         }
     }
     eventHandelar = (event) => {
         this.setState({
-            name: event.target.value
-       
-        })
+           name:event.target.value
+       })
     }
-        render() {
-            return (
-                <div>
-                    <input onChange={this.eventHandelar} value={this.state.name} type="text" placeholder="Write Anything"></input>
-                    {this.state.name ? <p>Hello,{this.state.name}</p> : ''}
-                </div>
-            )
-        }
-    
-   
+    render(){
+        return (
+            <div>
+            <input onChange={this.eventHandelar} value={this.state.name} type="text" placeholder="Write Anything"></input>
+            {this.state.name ? <p>Hello,{this.state.name}</p> : ''}
+            </div> 
+        )
+    }
 }
