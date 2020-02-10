a310c381d6d25c0d7e0fce73c6af11d7e7d68aa0
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-10 12:17:43 +0600

    External CSS style In JS File

---
+++ CSSwithCondition/ExternalCSSinJS.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import * as styles from './style'
+ const MyFunction = ()=>{
+
+    return (
+        <div>
+           <h1 style={styles.style}>Hello, My name is Ashikur Rohman</h1> 
+        </div>
+    )
+}
+export default MyFunction;
