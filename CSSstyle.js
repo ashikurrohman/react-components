e36f590ca948b099f5633345615f3cff99d048fa
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-10 11:59:08 +0600

    Internal CSS Style

---
+++ CSSstyle.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import style from './style.js'
+const myFunct = ()=>{
+    const styles = {
+        color:"red"
+    }
+    return (
+        <div>
+            <h1 style={styles}>Hello, My name is Ashikur Rohman</h1>
+        </div>
+    )
+}
+export default myFunct
