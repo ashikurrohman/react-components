2ef4bb9439bc1046564ff5c99f3be26738e031f8
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-12 11:46:40 +0600

    Conditional Rendering in Class Component

---
+++ ConditionalRendaring.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Myapp extends Component {
+    render() {
+        if(false){
+        return (
+            <div>
+                <h1>now, I am Happy</h1>
+            </div>
+        )
+        }else{
+            return (<div>
+            <h1>I am not good</h1>
+            </div>
+            )
+        }
+    }
+}
