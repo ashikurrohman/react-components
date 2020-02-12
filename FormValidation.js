bbdca8629dc1badcdc39c80fb2ddd9b106aed86c
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-12 22:37:48 +0600

    Form validation In React

---
+++ FormValidation.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name:""
+        }
+    
+    }
+    ChangeMet= (event) =>{
+        this.setState({[event.target.name]:event.target.value})
+    }
+    formSubmit=(event)=>{
+        alert(`My name is ${this.state.name}`)
+     }
+    render() {
+        return(
+            <div>
+                <div>
+                    <h1>Sign UP Here</h1>
+                </div>
+            <form onSubmit={this.formSubmit}>
+                <label>First Name</label>
+                <input  type="text"  name="name" value={this.state.name}onChange={this.ChangeMet}/><br/>
+                <label>Email Adress</label>
+                <input  type="email" name="email" onChange={this.ChangeMet}/><br/>
+                <label>Password</label>
+                <input  type="password" name="password" /><br/>
+                <input type="submit"/>
+            </form>
+    
+         </div>
+        )
+        
+    }
+}
+
+
