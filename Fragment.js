39a7be53a6424d4094e5f2f40579c1f738381e82
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-21 11:49:22 +0600

    Use of Fragment

---
+++ Fragment.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReactFrag extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h2> Use of Fragment</h2>
+            </React.Fragment>
+        )
+    }
+}
