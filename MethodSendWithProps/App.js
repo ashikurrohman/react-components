a14501a353002e4be81628c7d8309055371ca12a
Ashikur Rohman <42498986+ashikurrohman@users.noreply.github.com>
2020-02-22 00:06:22 +0600

    Update App.js

--- MethodSendWithProps/App.js
+++ MethodSendWithProps/App.js
@@ -1,1 +1,16 @@
 
+import React, { Component, createRef } from 'react'
+import ReactProps from './ReactProps';
+
+class Apps extends Component {
+  
+    render() {
+        return(
+          
+            <ReactProps/>
+              
+ )
+    }
+}
+
+export default Apps
